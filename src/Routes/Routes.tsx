9833950f09ef2cea20a92d9f407833add5dfb529
=KalZera <=kalex.codr@gmail.com>
2021-05-22 00:02:17 -0300

    criando arquivo de rotas do aplicativo

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
+
+export const PathsDefault = {
+	'page-home': '/',
+};
+
+export const Routes: FunctionComponent = () => {
+	return <div />;
+};
