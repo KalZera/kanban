4ead6ac22f41da5c35e0387ea3feadd4539d92c4
=KalZera <=kalex.codr@gmail.com>
2021-05-22 00:08:04 -0300

    configurando as rotas para que as paginas sejam acessadas pelo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import { Routes } from 'Routes';
 
 function App() {
-  return <div className="App">teste</div>;
+	return <Routes />;
 }
 
 export default App;
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,10 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
+import { PageHome } from 'Pages';
 
 export const PathsDefault = {
 	'page-home': '/',
 };
 
 export const Routes: FunctionComponent = () => {
-	return <div />;
+	return (
+		<Switch>
+			<Route exact path={PathsDefault['page-home']} component={PageHome} />
+			<Redirect exact from="/" to={PathsDefault['page-home']} />
+		</Switch>
+	);
 };
