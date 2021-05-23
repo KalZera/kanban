16b119a5ac9e7b262f062008039437dce299279e
=KalZera <=kalex.codr@gmail.com>
2021-05-23 14:22:31 -0300

    adicionando props ao componente coluna de forma estatica

--- src/Components/Column/Column.tsx
+++ src/Components/Column/Column.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Container, TitleColumn } from './styles';
 import { Item } from './Components';
 import { Button } from 'Components';
-export const Column: React.FC = () => {
+
+interface Item {
+	nameItem: string;
+	tags?: string[];
+}
+
+interface Props {
+	Column: { id: number; title: string; items: Item[] };
+}
+
+export const Column: FunctionComponent<Props> = ({ Column }) => {
 	return (
 		<Container>
-			<TitleColumn> To Do </TitleColumn>
-			<Item />
-			<Item />
-			<Item />
+			<TitleColumn> {Column.title} </TitleColumn>
+			{Column.items.map(item => (
+				<Item key={item.nameItem} item={item} />
+			))}
 			<Button />
 		</Container>
 	);
