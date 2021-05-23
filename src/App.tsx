167b6b903e4803bcad4a2df4cf26728c617215e6
=KalZera <=kalex.codr@gmail.com>
2021-05-23 15:02:19 -0300

    adicionando store ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes } from 'Routes';
 import { GlobalStyle } from 'Styles';
+import { Provider } from 'react-redux';
+import { store } from 'store';
 
 const App: React.FC = () => {
 	return (
 		<>
-			<GlobalStyle />
-			<Routes />
+			<Provider store={store}>
+				<GlobalStyle />
+				<Routes />
+			</Provider>
 		</>
 	);
 };
