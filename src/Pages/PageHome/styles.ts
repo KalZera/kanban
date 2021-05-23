2e2db171d653f3ba0d24fafa555e5379f32bfb2c
=KalZera <=kalex.codr@gmail.com>
2021-05-23 14:28:35 -0300

    criando arquivo de estilos para a pagina home

---
+++ src/Pages/PageHome/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+	margin-top: 2rem;
+	margin-bottom: 2rem;
+	font-size: 32px;
+	font-weight: 700;
+`;
+
+export const ColumnGroups = styled.div`
+	display: flex;
+`;
+
+export const ButtonAddColumn = styled.div`
+	height: fit-content;
+	display: flex;
+	min-width: 315px;
+	background-color: blue;
+	padding: 1rem;
+	margin-left: 0.5rem;
+`;
