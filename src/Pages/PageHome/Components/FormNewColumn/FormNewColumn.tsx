7aa6101a0b245dab55f002271118b5e5d548256a
=KalZera <=kalex.codr@gmail.com>
2021-05-24 12:20:39 -0300

    adicionando dispatch dinamico ao form

--- src/Pages/PageHome/Components/FormNewColumn/FormNewColumn.tsx
+++ src/Pages/PageHome/Components/FormNewColumn/FormNewColumn.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 export const FormNewColumn: React.FC = () => {
+	const [column, setColumn] = useState<string>('');
 	const dispatch = useDispatch();
 	const AddColumn = () => {
-		dispatch({ type: 'ADD_COLUMN', payload: 'Nova coluna' });
+		dispatch({ type: 'ADD_COLUMN', payload: column });
 	};
 	return (
 		<>
 			<div>Nova Coluna</div>
-			<input type="text" />
+			<input type="text" value={column} onChange={e => setColumn(e.target.value)} />
 			<button onClick={AddColumn}>Adicionar Coluna</button>
 		</>
 	);
