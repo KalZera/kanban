5f6346b3dd6ca0a2b143bcb1086f6741dd94891c
=KalZera <=kalex.codr@gmail.com>
2021-05-24 11:54:04 -0300

    criando arquivos do componente modal

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export const Modal: React.FC = () => {
+	return <div />;
+};
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export * from './Column';
 export * from './Button';
-export * from './Tag'
+export * from './Tag';
+export * from './Modal';
