3d350061112ecfc01dd1cae9e739d921370495af
=KalZera <=kalex.codr@gmail.com>
2021-05-24 11:33:45 -0300

    deixando os primeiros valores estaticos para testes em pagina.

--- src/store/Reducers/Column.ts
+++ src/store/Reducers/Column.ts
@@ -1,18 +1,29 @@
-import { title } from 'process';
+type Type = 'default' | 'custom';
+export interface ColumnType {
+	id: number;
+	title: string;
+	color?: string;
+	type?: Type;
+}
 
-const INITIAL_STATE: any = {
+export interface StateProps {
+	columns: ColumnType[];
+}
+
+const INITIAL_STATE: StateProps = {
 	columns: [
-		{ title: 'To do', color: 'blue' },
-		{ title: 'In Progress', color: 'green' },
-		{ title: 'Done', color: 'gray' },
+		{ id: 0, title: 'To do', color: 'blue' },
+		{ id: 1, title: 'In Progress', color: 'green' },
+		{ id: 2, title: 'Done', color: 'gray' },
 	],
 };
+type Action = { type: 'ADD_COLUMN'; payload: string };
 
-export const Column = (state = INITIAL_STATE, action: { type: string; payload: any }) => {
+export const ColumnReducer = (state: StateProps = INITIAL_STATE, action: Action) => {
 	switch (action.type) {
 		case 'ADD_COLUMN':
-			return { ...state, columns: [...state.columns, { title: action.payload.title }] };
+			return { ...state, columns: [...state.columns, { id: state.columns.length, title: action.payload }] };
 		default:
-			return INITIAL_STATE;
+			return state;
 	}
 };
