aa7b2acd3f428bd7ae47a5c81f54c1538acf4365
=KalZera <=kalex.codr@gmail.com>
2021-05-24 12:03:55 -0300

    adicionando conteúdo no componente modal de forma estatica

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Modal as ModalB } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import { Container } from './styles';
+interface Props {
+	open: boolean;
+	handleClose: () => void;
+}
 
-export const Modal: React.FC = () => {
-	return <div />;
+export const Modal: React.FC<Props> = ({ open, handleClose }) => {
+	return (
+		<ModalB show={open} onHide={handleClose}>
+			<ModalB.Header closeButton>
+				<ModalB.Title>Modal title</ModalB.Title>
+			</ModalB.Header>
+
+			<ModalB.Body>
+				<p>Modal body text goes here.</p>
+			</ModalB.Body>
+		</ModalB>
+	);
 };
