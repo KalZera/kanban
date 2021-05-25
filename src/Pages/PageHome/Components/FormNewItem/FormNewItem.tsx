801648b0036969d7d0d7127c5d58b6eef01af2dc
=KalZera <=kalex.codr@gmail.com>
2021-05-25 18:34:03 -0300

    adicionando items para uma coluna específica

--- src/Pages/PageHome/Components/FormNewItem/FormNewItem.tsx
+++ src/Pages/PageHome/Components/FormNewItem/FormNewItem.tsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { ColumnType } from 'store/Reducers';
 
-export const FormNewItem: React.FC = () => {
+interface Props {
+	columns: ColumnType[];
+}
+
+export const FormNewItem: React.FC<Props> = ({ columns }) => {
 	const [item, setItem] = useState<string>('');
-	// const [column, setColumn] = useState<string>('');
+	const [column, setColumn] = useState<number>(0);
 	const dispatch = useDispatch();
 	const AddItem = () => {
-		dispatch({ type: 'ADD_ITEM', payload: item });
+		dispatch({ type: 'ADD_ITEM', payload: { idColumn: column, item } });
+	};
+	const changeSelect = (event: any) => {
+		setColumn(parseInt(event.target.value));
 	};
 	return (
 		<>
 			<div>Novo Item</div>
 			<input type="text" value={item} onChange={e => setItem(e.target.value)} />
+			<select onChange={changeSelect}>
+				{columns.map(column => (
+					<option key={column.id} value={column.id}>
+						{column.title}
+					</option>
+				))}
+			</select>
 			<button onClick={AddItem}>Adicionar Item</button>
 		</>
 	);
