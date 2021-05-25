80ef8ce84170d63c8d265e9cf9aebe8b24a4b57c
=KalZera <=kalex.codr@gmail.com>
2021-05-25 20:39:13 -0300

    refact da pagina home adicionando os componentes, add funcionalidade de modal e exibição das colunas

--- src/Pages/PageHome/PageHome.tsx
+++ src/Pages/PageHome/PageHome.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { Content } from 'Styles';
+import { FiPlus } from 'react-icons/fi';
+
+import { Title, ColumnGroups, ButtonAddColumn } from './styles';
+import { Column, Button, Modal } from 'Components';
+import { FormNewColumn, FormNewItem } from './Components';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { ColumnType } from 'store/Reducers';
+import { RootState } from 'store';
 
 export const PageHome: React.FC = () => {
-	return <div> Page Home </div>;
+	const { columns } = useSelector((state: RootState) => state.columns);
+	const { modal } = useSelector((state: RootState) => state.modal);
+	const { items } = useSelector((state: RootState) => state.items);
+	const dispatch = useDispatch();
+	const AddColumn = () => {
+		dispatch({ type: 'TOGGLE_MODAL_COLUMN' });
+	};
+	const ChoseForm = (): JSX.Element => {
+		switch (modal.form) {
+			case 'column':
+				return <FormNewColumn />;
+			case 'item':
+			default:
+				return <FormNewItem columns={columns} />;
+		}
+	};
+
+	return (
+		<Content>
+			<Title>Page Home</Title>
+			<ColumnGroups>
+				{columns.map((column: ColumnType) => (
+					<Column key={column.title} column={column} items={items} />
+				))}
+				<ButtonAddColumn>
+					<Button icon={<FiPlus />} onClick={AddColumn} text="Adicionar outra coluna" />
+				</ButtonAddColumn>
+			</ColumnGroups>
+			<Modal open={modal.open} handleClose={AddColumn} title="Adicionar">
+				<ChoseForm />
+			</Modal>
+		</Content>
+	);
 };
