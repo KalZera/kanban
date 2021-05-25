b909839d13ab9cc8019d3975900f5dcf5f6c56d9
=KalZera <=kalex.codr@gmail.com>
2021-05-25 20:34:49 -0300

    refatorando componente botão adicionando novos estilos

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 import { Container } from './styles';
 
+type styleType = 'button' | 'link';
+
 interface Props {
 	onClick: () => void;
 	text: string;
 	icon?: JSX.Element;
+	styleButton?: styleType;
 }
 
-export const Button: React.FC<Props> = ({ onClick, icon, text }) => {
+export const Button: React.FC<Props> = ({ onClick, icon, text, styleButton }) => {
 	return (
-		<Container onClick={onClick}>
+		<Container onClick={onClick} typestyle={styleButton || 'link'}>
 			{!!icon && icon} {text}
 		</Container>
 	);
--- src/Components/Button/styles.ts
+++ src/Components/Button/styles.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.button`
-	background-color: transparent;
-	border: none;
+type typeButton = 'button' | 'link';
+export const Container = styled.button<{ typestyle: typeButton }>`
+	${props => {
+		if (props.typestyle === 'button') {
+			return { backgroundColor: '#06a94d', padding: '0.35rem 1rem' };
+		} else {
+			return { backgroundColor: 'transparent', padding: '0.35rem' };
+		}
+	}}
 	color: white;
+	border: none;
+	border-radius: 5px;
 	text-align: initial;
 	margin-top: 0.5rem;
 	margin-bottom: 0.5rem;
