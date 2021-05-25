cc9b060de40c702f55e7c287a77b39f9ea39b207
=KalZera <=kalex.codr@gmail.com>
2021-05-25 17:54:36 -0300

    adicionando formulário de item de forma estatica

--- src/Pages/PageHome/Components/FormNewItem/FormNewItem.tsx
+++ src/Pages/PageHome/Components/FormNewItem/FormNewItem.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 export const FormNewItem: React.FC = () => {
-	return <div />;
+	const [item, setItem] = useState<string>('');
+	// const [column, setColumn] = useState<string>('');
+	const dispatch = useDispatch();
+	const AddItem = () => {
+		dispatch({ type: 'ADD_ITEM', payload: item });
+	};
+	return (
+		<>
+			<div>Novo Item</div>
+			<input type="text" value={item} onChange={e => setItem(e.target.value)} />
+			<button onClick={AddItem}>Adicionar Item</button>
+		</>
+	);
 };
