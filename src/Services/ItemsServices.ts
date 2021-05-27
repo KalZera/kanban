562a3016240f32fedcdf765865fed739893104ef
=KalZera <=kalex.codr@gmail.com>
2021-05-27 11:26:16 -0300

    refatorando os services para que as regras sejam efetuadas nele

--- src/Services/ItemsServices.ts
+++ src/Services/ItemsServices.ts
@@ -1,7 +1,10 @@
 //onde será feita todas as regras e alteração de dados.
+import { ItemsType } from 'store/Reducers';
 
-const ChangeItem = () => {
-	return true;
+const ChangeItem = (items: ItemsType[], itemToChange: ItemsType, item: string, column: number): ItemsType[] => {
+	const newItems = items.filter(item => item.id !== itemToChange.id);
+	const itemChange = [...newItems, { ...itemToChange, idColumn: column, title: item }];
+	return itemChange;
 };
 
 export const ItemsService = { ChangeItem };
