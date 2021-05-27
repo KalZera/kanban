d99880e701d1736ff3a89d756a18107a99a185ea
=KalZera <=kalex.codr@gmail.com>
2021-05-27 18:12:30 -0300

    refatorando componente item transformando ele em um componente a parte

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Container, TitleItem, SectionTags } from './styles';
 import { Tag } from 'Components';
 import { ItemsType } from 'store/Reducers/Items';
+import { TagType } from 'store/Reducers/Tags';
+import { RootState } from 'store';
 
 interface Props {
 	item: ItemsType;
@@ -11,16 +13,16 @@ interface Props {
 
 export const Item: FunctionComponent<Props> = ({ item }) => {
 	const dispatch = useDispatch();
+	const { tags } = useSelector((state: RootState) => state.tag);
 	const changeItem = () => {
 		dispatch({ type: 'SELECT_ITEM', payload: item });
 		dispatch({ type: 'TOGGLE_MODAL_ITEM' });
 	};
+
 	return (
 		<Container onClick={changeItem}>
 			<TitleItem>{item.title}</TitleItem>
-			<SectionTags>
-				<Tag>Tag 1</Tag>
-			</SectionTags>
+			<SectionTags>{tags.map((tag: TagType) => tag.id === item.tag && <Tag key={tag.id} textTag={tag.title} />)}</SectionTags>
 		</Container>
 	);
 };
