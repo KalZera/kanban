e9d442bda9d1e67f04a8a599e80b4057e2a666fb
=KalZera <=kalex.codr@gmail.com>
2021-05-27 10:13:21 -0300

    refact componente select para aceitar valores

--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -4,11 +4,12 @@ import { Container } from './styles';
 
 interface Props {
 	options: { name: string; value: string | number }[];
+	value: number | string;
 	onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 }
-export const Select: React.FC<Props> = ({ options, onChange }) => {
+export const Select: React.FC<Props> = ({ options, value, onChange }) => {
 	return (
-		<Container onChange={onChange}>
+		<Container onChange={onChange} value={value}>
 			{options.map(option => (
 				<option key={option.value} value={option.value}>
 					{option.name}
